b4631cfb5ca93f9471a6c0d82e1e5db50c6104ee
Nikhil Deshmukh <nikhil77d@gmail.com>
2023-04-18 22:23:16 +0530

    Updates

---
+++ precache-manifest.e6970c0d09069e8aed72e94ab2151c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b0f56018d00a6cf49f8",
+    "url": "/react_courses_project/static/css/main.9b000ac4.chunk.css"
+  },
+  {
+    "revision": "7b0f56018d00a6cf49f8",
+    "url": "/react_courses_project/static/js/main.7b0f5601.chunk.js"
+  },
+  {
+    "revision": "d4a414ccabb08da35c55",
+    "url": "/react_courses_project/static/css/1.2e9705bb.chunk.css"
+  },
+  {
+    "revision": "d4a414ccabb08da35c55",
+    "url": "/react_courses_project/static/js/1.d4a414cc.chunk.js"
+  },
+  {
+    "revision": "9a3fcb112d752ce87734",
+    "url": "/react_courses_project/static/js/runtime~main.9a3fcb11.js"
+  },
+  {
+    "revision": "147a9526ff8fdcb78f07d8d5319a9482",
+    "url": "/react_courses_project/index.html"
+  }
+];
